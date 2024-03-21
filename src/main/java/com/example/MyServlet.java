875cfe416e5912ecacaa9ba2fc7aacf12b4994ec
root <root@ip-172-31-15-53.us-west-2.compute.internal>
2024-03-21 08:28:51 +0000

    sample java project-first commit

---
+++ src/main/java/com/example/MyServlet.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, Maven Web Application!</h1>");
+        out.println("</body></html>");
+    }
+}
+
